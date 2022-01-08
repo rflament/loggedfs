b6093c10a93190c469cf9bd6736a8a6089874cf8
Rémi <rflament.mtl@gmail.com>
2022-01-08 14:09:35 +0100

    food2

--- test.sh
+++ test.sh
@@ -42,14 +42,14 @@ echo "bar" >> $TEST_DIR/foo2 && ls $TEST_DIR
 sudo umount $TEST_DIR
 
 # make sure the actions have been logged in syslog
-COUNT=$(grep -c "write 4 bytes to $TEST_DIR/foo at offset 0" /var/log/syslog)
+COUNT=$(grep -c "write 4 bytes to $TEST_DIR/foo2 at offset 0" /var/log/syslog)
 if [ "$COUNT" -ne "1" ]; then
   echo "write should have been logged in syslog"
   exit 1
 fi
 
 # make sure the actions have not been logged in log file
-COUNT=$(grep -c "write 4 bytes to $TEST_DIR/foo at offset 0" ~/log.txt)
+COUNT=$(grep -c "write 4 bytes to $TEST_DIR/foo2 at offset 0" ~/log.txt)
 if [ "$COUNT" -ne "0" ]; then
   echo "nothing should have been logged in log file"
   exit 1
