a34899d8fab6b1429b45440b51176a64a464e06c
Rémi <rflament.mtl@gmail.com>
2022-01-08 14:07:43 +0100

    test

--- test.sh
+++ test.sh
@@ -15,7 +15,7 @@ echo "bar" >> $TEST_DIR/foo && ls $TEST_DIR
 sudo umount $TEST_DIR
 
 # make sure the actions have been logged in the log file
-COUNT=$(grep -c "write 4 bytes to $TEST_DIR/foo at offset 0"  ~/log.txt)
+COUNT=$(grep -c "write 4 bytes to $TEST_DIR/foo at offset 0" ~/log.txt)
 if [ "$COUNT" -ne "1" ]; then
   echo "write should have been logged in file"
   exit 1
@@ -25,6 +25,32 @@ fi
 COUNT=$(grep -c "write 4 bytes to $TEST_DIR/foo at offset 0" /var/log/syslog)
 if [ "$COUNT" -ne "0" ]; then
   echo "nothing should have been logged in syslog"
-  grep loggedfs /var/log/syslog
+  exit 1
+fi
+
+rm ~/log.txt
+
+####################################################################
+## Test 2: log actions to syslog
+####################################################################
+
+# launch loggedfs
+./loggedfs $TEST_DIR
+
+# create file
+echo "bar" >> $TEST_DIR/foo2 && ls $TEST_DIR
+sudo umount $TEST_DIR
+
+# make sure the actions have been logged in syslog
+COUNT=$(grep -c "write 4 bytes to $TEST_DIR/foo at offset 0" /var/log/syslog)
+if [ "$COUNT" -ne "1" ]; then
+  echo "write should have been logged in syslog"
+  exit 1
+fi
+
+# make sure the actions have not been logged in log file
+COUNT=$(grep -c "write 4 bytes to $TEST_DIR/foo at offset 0" ~/log.txt)
+if [ "$COUNT" -ne "0" ]; then
+  echo "nothing should have been logged in log file"
   exit 1
 fi
