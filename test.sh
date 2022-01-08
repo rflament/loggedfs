fd868237a0e98079f4f2ecd44d482ff87a7cc0d0
Rémi <rflament.mtl@gmail.com>
2022-01-08 13:39:46 +0100

    test

---
+++ test.sh
@@ -0,0 +1,23 @@
+TEST_DIR=/tmp/test-loggedfs
+mkdir $TEST_DIR
+
+####################################################################
+## Test 1: log actions in a file
+####################################################################
+
+# launch loggedfs
+./loggedfs -l ~/log.txt $TEST_DIR
+
+# create file
+cat "bar" >> $TEST_DIR/foo && ls $TEST_DIR
+sudo umount $TEST_DIR
+
+# make sure the actions have been logged in the log file
+cat ~/log.txt
+
+# make sure the actions have not been logged in syslog
+COUNT=$(grep -c grep loggedfs /var/log/syslog)
+if [ $COUNT -ne "0" ]; then
+  echo "nothing should have been logged in syslog"
+  exit 1
+fi
